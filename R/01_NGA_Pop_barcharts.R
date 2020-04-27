6837b5f74b98bcd649ba29ff7aa089dbf22b2e40
pgault <patrickgault@gmail.com>
2020-04-27 12:38:24 -0400

    bar chart updates

--- R/01_NGA_Pop_barcharts.R
+++ R/01_NGA_Pop_barcharts.R
@@ -25,33 +25,32 @@ library(scales)
       "Urbaness.Class" = MunicipalLevelType) %>% 
     mutate(label = percent(PctPoverty %>% round(2)))
 
-
 # MUNGE  ------------------------------------------------------------------
 # No need to reshape
 
 # PLOT --------------------------------------------------------------------
-
-
+  
 ## Create Bar Charts
 # WorldPop
   wp_df %>% 
     select(-Population) %>% 
     # right now this is sorted on the pop_poverty, below. Change as needed.
-    ggplot(aes(x = fct_reorder(Urbaness.Class, pop_poverty))) +
+    ggplot(aes(x = fct_reorder(Urbaness.Class, ZoneID))) +
     geom_col(data = wp_df, aes(y = Population),
       fill = "#a6cee3") +
     coord_flip() +
     theme_minimal() + 
+    theme(plot.title = element_text(hjust = 0.5, face="bold")) +
     scale_y_continuous(labels = scales::comma) +
     geom_col(aes(y = pop_poverty), fill = "#1f78b4") +
     geom_text(aes(y = pop_poverty, label = label),
       hjust = -0.25, size = 3) +
     theme(panel.grid.major.y = ggplot2::element_blank()
     ) +
-    labs(x = "GHS Urbaness Class", y = NULL, 
-      title = "Nigeria: GHS 2010 Population in Poverty")
+    labs(x = "Population Density (Sq. Km)", y = NULL, 
+      title = "Nigeria: 2010 Percent Population in Poverty - WorldPop")
 
-ggsave("Awesome_plot0.pdf", plot = last_plot(),
+ggsave("Nigeria_2010PctPoverty_WorldPop.pdf", plot = last_plot(),
   height = 4,
   width = 8, 
   useDingbats = FALSE)
@@ -60,22 +59,23 @@ ggsave("Awesome_plot0.pdf", plot = last_plot(),
 # PLOT GHS
 
   ghs_df %>% 
-    select(-Population) %>% 
-    ggplot(aes(x = fct_reorder(Urbaness.Class, pop_poverty))) +
+    #select(-Population) %>% 
+    ggplot(aes(x = fct_reorder(Urbaness.Class, ZoneID))) +
     geom_col(data = ghs_df, aes(y = Population),
       fill = "#a6cee3") +
     coord_flip() +
     theme_minimal() + 
+    theme(plot.title = element_text(face="bold")) +
     scale_y_continuous(labels = scales::comma) +
     geom_col(aes(y = pop_poverty), fill = "#1f78b4") +
     geom_text(aes(y = pop_poverty, label = label),
-      hjust = -0.25, size = 3) +
+      hjust = -0.15, size = 3) +
     theme(panel.grid.major.y = ggplot2::element_blank()
     ) +
-    labs(x = NULL, y = NULL, 
-      title = "Nigeria: GHS 2010 Population in Poverty")
+    labs(x = "GHS Urbaness Class", y = NULL, 
+      title = "Nigeria: 2010 Percent Population in Poverty - GHS Degrees of Urbaness")
 
-ggsave("Awesome_plot1.pdf", plot = last_plot(),
+ggsave("Nigeria_2010PctPoverty_GHS.pdf", plot = last_plot(),
   height = 4,
-  width = 8, 
+  width = 10, 
   useDingbats = FALSE)
